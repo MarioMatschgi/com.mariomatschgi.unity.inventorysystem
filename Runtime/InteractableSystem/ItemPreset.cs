e59d626a446d9fba9b4e1163210d1461fe6618ef
Mario Elsnig <46848943+MarioMatschgi@users.noreply.github.com>
2020-04-04 19:17:27 +0200

    Update ItemPreset.cs

--- Runtime/InteractableSystem/ItemPreset.cs
+++ Runtime/InteractableSystem/ItemPreset.cs
@@ -3,12 +3,12 @@
 namespace MM.Systems.InventorySystem
 {
     [System.Serializable]
-    [CreateAssetMenu(fileName = "New Item data", menuName = "ScriptableObjects/MM InventorySystem/ItemPreset", order = 0)]
+    [CreateAssetMenu(fileName = "New Item preset", menuName = "ScriptableObjects/MM InventorySystem/ItemPreset", order = 0)]
     public class ItemPreset : ScriptableObject
     {
         [Header("ItemPreset")]
-        public new string name = "New Item";
-        public string description = "This is a new Item";
+        public new string name = "New Item preset";
+        public string description = "This is a new Item preset";
         [Space]
         public Sprite sprite;
         public int stackSize = 1;
